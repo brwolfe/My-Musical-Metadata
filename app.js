61bfea4cde9a8c79766242b81c2d0d7f4a0e8495
ide user ide_carrboro <dooley.sarah.e@gmail.com>
2012-05-02 21:58:57 +0000

    app.js

--- app.js
+++ app.js
@@ -123,34 +123,6 @@ app.get('/albums/',         // TODO: change to suit your URI design.
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// GET a list of albums by search criteria. ///////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-//app.get('/albums/?datePublished',         // TODO: change to suit your URI design. 
-//  function(req, res) {
-//
-//    var item_type = 'album'; // TODO: change to the type of item you want.
-//    var item = req.body.item; //Get year info from search form, set as a variable to query db.
-//
-    // Get all items of the specified type from the database.
-//    db.getSome(item_type, req.query.item, function(err, items) {
-//  
-      // If there was a database error, return an error status.
-//      if (err) { res.send(err, 500); } 
-//
-      // Otherwise, use the returned data to render an HTML page.
-//      else {
-//        res.render(
-//          'list-albums',   // TODO: change to the name of your HTML template.
-//          { items: items, item: item }
-//        );
-//      }
-//    });
-//  }
-//);
-
-
-
-////////////////////////////////////////////////////////////////////////////////
 // GET a single album resource, which a list of dependent songs. ///////////////
 // Get those too as related items. /////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -222,49 +194,6 @@ app.get('/songs/',         // TODO: change to suit your URI design.
   }
 );
 
-////////////////////////////////////////////////////////////////////////////////
-// GET the allsongs resource, which is a list of songs./////////////////////////
-// Get the dependent type artist too.///////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-//app.get('/songs/',       // TODO: change to suit your URI design.
-//  function(req, res) {
-//
-//    var item_type = 'song'; // TODO: change to the type of item you want.
-//
-    // Get the item ID from the URI.
-//    var item_id = req.params.id;
-//  
-    // Get one item of the specified type, identified by the item ID.
-//    db.getOne(item_type, item_id, function(err, item) {
-//        
-//      // If there was a database error, return an error status.
-//      if (err) {
-//        if (err.error == 'not_found') { res.send(404); }
-//        else { res.send(err, 500); }
-//      } 
-//
-      // Otherwise, get the items potentially related to this item.
-//      else {
-//        
-//        var related_type = 'artist'; // TODO: change to type of related item.
-//
-        // Get all items of the specified related type.
-//        db.getAll(related_type, function(err, items) {
-//
-          // If there was a database error, return an error status.
-//          if (err) { res.send(err, 500); } 
-//
-          // Otherwise, use the returned data to render an HTML page.
-//          else { res.render(
-//              'list-songs', // TODO: change to name of your HTML template.
-//              { items: items, related_items: items }
-//            );
-//          }
-//        });
-//      }
-//    });
-//  }
-//);
 
 ////////////////////////////////////////////////////////////////////////////////
 // GET a single song resource with a list of related albums. ///////////////////
